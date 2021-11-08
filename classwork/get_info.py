28c53bd1642926d4bc9a4517fe7d195a1a98d9a1
Юрій Середюк <yura2590@gmail.com>
2021-11-08 21:29:11 +0200

    add: uml diagram and interface structure files

--- classwork/get_info.py
+++ classwork/get_info.py
@@ -36,4 +36,47 @@ for row in rezults:
         print(item, end=' ')
     else:
         print()
+# 
+print('\tTASK 5:')
+query = """select  e.first_name,e.last_name from employee e
+            where  e.id in(
+            select o.employee_id from orders o 
+            where o.city_id in(
+            select  c.id from  city c where c.city_name = 'Madrid'));"""
+cursor.execute(query)
+rezults = cursor.fetchall()
+for row in rezults:
+    for item in row:
+        print(item, end=' ')
+    else:
+        print()
+# 
+print('\tTASK 6:')
+query = """select  e.first_name,e.last_name, count(o.date_of_order) from employee e
+            left join orders o on e.id = o.employee_id 
+            where extract(year from o.date_of_order) = 2020 and o.date_of_order >'2020-09-03'
+            group  by e.first_name, e.last_name;"""
+cursor.execute(query)
+rezults = cursor.fetchall()
+for row in rezults:
+    for item in row:
+        print(item, end=' ')
+    else:
+        print()     
+# 
+print('\tTASK 7:')
+query = """select  c.first_name, c.last_name, count(product_id) as "Count", sum(price) as "Sum" from customer c 
+            left join orders o on o.customer_id = c.id
+            where o.customer_id in(
+            select o2.customer_id from orders o2 where o2.product_id in (
+            select p.id from product p where p.product_name = 'Tofu'))
+            group  by c.first_name, c.last_name,  c.id;"""
+cursor.execute(query)
+rezults = cursor.fetchall()
+for row in rezults:
+    for item in row:
+        print(item, end=' ')
+    else:
+        print()   
+  
 Connector.closeDB(connection, cursor)
---
+++ classwork/interface/customer.py
@@ -0,0 +1,11 @@
+from lib.profile import Profile
+
+class Customer:
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        
+    def edit_customer(self):
+        pass
+
+    
